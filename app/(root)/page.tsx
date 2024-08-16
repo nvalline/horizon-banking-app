d1adc5927e3060e0ab7997ef28ee05d78391499a
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-16 09:55:43 -0600

    implement HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
 
 const Home = () => {
+	const loggedIn = { firstName: 'Genevieve' };
+
 	return (
-		<div>
-			<p>Home</p>
-		</div>
+		<section className='home'>
+			<div className='home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcome'
+						user={loggedIn?.firstName || 'Guest'}
+						subtext='Conveniently access and handle your account and transactions.'
+					/>
+				</header>
+			</div>
+		</section>
 	);
 };
 
