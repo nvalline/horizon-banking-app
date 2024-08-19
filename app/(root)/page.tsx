b241492b90aeec6b55a9c385fbfe0fa21f5ceb88
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-19 15:10:31 -0600

    implement RightSidebar & BankCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 // Components
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-	const loggedIn = { firstName: 'Genevieve' };
+	const loggedIn = {
+		firstName: 'Genevieve',
+		lastName: 'LeTour',
+		email: 'g.letour@tour.fr'
+	};
 
 	return (
 		<section className='home'>
@@ -21,7 +26,13 @@ const Home = () => {
 						totalCurrentBalance={2489.65}
 					/>
 				</header>
+				RECENT TRANSACTIONS
 			</div>
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 2423.32 }, { currentBalance: 938.25 }]}
+			/>
 		</section>
 	);
 };
