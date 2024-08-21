77948416b35bebfb2e0efd5d9c2aee524c24683f
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-21 14:34:48 -0600

    implement signup functionality with appwrite

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,7 @@
 // Components
 import AuthForm from '@/components/AuthForm';
 
-const SignUp = () => {
+const SignUp = async () => {
 	return (
 		<section className='flex-center size-full max-sm:px-6'>
 			<AuthForm type='sign-up' />
