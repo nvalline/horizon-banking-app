c2fe0137590dfbd0db7f8579d4846c9008db33c1
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-21 14:42:41 -0600

    implement signin functionality with appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,9 +5,13 @@ import { createAdminClient, createSessionClient } from '../server/appwrite';
 import { cookies } from 'next/headers';
 import { parseStringify } from '../utils';
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
 	try {
-		// Authenticate user
+		const { account } = await createAdminClient();
+
+		const response = await account.createEmailPasswordSession(email, password);
+
+		return parseStringify(response);
 	} catch (error) {
 		console.log('Error:', error);
 	}
