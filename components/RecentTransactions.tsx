286d3ba96cf6764818e106f570bff327bab4a23e
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-27 12:35:30 -0600

    Implement TransactionTable & styles

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import BankTabItem from './BankTabItem';
 import BankInfo from './BankInfo';
+import TransactionsTable from './TransactionsTable';
 
 const RecentTransactions = ({
 	accounts,
@@ -49,6 +50,8 @@ const RecentTransactions = ({
 							appwriteItemId={appwriteItemId}
 							type='full'
 						/>
+
+						<TransactionsTable transactions={transactions} />
 					</TabsContent>
 				))}
 			</Tabs>
