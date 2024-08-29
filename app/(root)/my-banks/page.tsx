5b2e63bc2f79f97d19b161280ee55f77b756199b
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-08-29 11:59:03 -0600

    Implement My Banks & Copy

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { getAccounts } from '@/lib/actions/bank.actions';
 
-const MyBanks = () => {
-	return <div>MyBanks</div>;
+// Components
+import HeaderBox from '@/components/HeaderBox';
+import BankCard from '@/components/BankCard';
+
+const MyBanks = async () => {
+	const loggedIn = await getLoggedInUser();
+	const accounts = await getAccounts({ userId: loggedIn.$id });
+
+	return (
+		<section className='flex'>
+			<div className='my-banks'>
+				<HeaderBox
+					title='My Bank Accounts'
+					subtext='Manage your banking activities.'
+				/>
+
+				<div className='space-y-4'>
+					<h2 className='header-2'>Your Cards</h2>
+					<div className='flex flex-wrap gap-6'>
+						{accounts &&
+							accounts.data.map((account: Account) => (
+								<BankCard
+									key={account.id}
+									account={account}
+									userName={loggedIn?.firstName}
+								/>
+							))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default MyBanks;
